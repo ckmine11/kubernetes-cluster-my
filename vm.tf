294ab92a67eb7c56c1be2f6e2be17d57961557b4
ckmine11 <167895891+ckmine11@users.noreply.github.com>
2024-07-12 11:41:53 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -129,66 +129,7 @@ resource "vsphere_virtual_machine" "Master1" {
     }
   }
 }
-#### Master2 VM CREATION ####
-# Set vm parameters
-resource "vsphere_virtual_machine" "Master2" {
-  name             = var.Master2_VM_NAME
-  num_cpus         = var.CPUS
-  memory           = var.Memory
-  datastore_id     = data.vsphere_datastore.datastore.id
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
-  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
-  firmware         = data.vsphere_virtual_machine.template.firmware
-  # Set network parameters
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
 
-  # Use a predefined vmware template as main disk
-  disk {
-    label = "vm-one.vmdk"
-    size  = var.VMDISK_SIZE
-  }
-
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = var.Master2_Host_Name
-        domain    = var.Domain_Name
-      }
-
-      network_interface {
-        ipv4_address    = var.Master2_VM_IP
-        ipv4_netmask    = var.NETMASK
-        dns_server_list = ["${var.DNS_SERVER}"]
-      }
-
-      ipv4_gateway = var.Gateway
-    }
-  }
-  provisioner "remote-exec" {
-    script     = "scripts/haproxy2.sh"
-    on_failure = continue
-    connection {
-      type     = "ssh"
-      user     = "root"
-      password = var.vm_password
-      host     = var.Master2_VM_IP
-
-    }
-  }
-  provisioner "remote-exec" {
-    script = "scripts/kube.sh"
-    connection {
-      type     = "ssh"
-      user     = "root"
-      password = var.vm_password
-      host     = var.Master2_VM_IP
-    }
-  }
   provisioner "remote-exec" {
     script = "scripts/kube.sh"
     connection {
